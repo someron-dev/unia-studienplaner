4e8ddd47d69d7b1d4870fdf91cefa70adc00dcd2
someron <git@someron.de>
2026-03-28 22:20:19 +0100

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    let number = $state(1);
+
+    function increment() {
+        number++;
+    }
+</script>
+
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<button onclick={increment}>Zahl: {number}</button>
